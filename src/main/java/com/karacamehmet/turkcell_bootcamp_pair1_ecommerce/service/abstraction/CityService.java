dc778f62e0d655c256be9053934ce856e3341845
karaca <mekaraca18@gmail.com>
2024-03-07 21:59:21 +0300

    ...

--- src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/abstraction/CityService.java
+++ src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/abstraction/CityService.java
@@ -1,4 +1,18 @@
 package com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.abstraction;
 
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.model.City;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CityService {
+    List<City> getAll();
+
+    Optional<City> getById(int id);
+
+    void add(City city);
+
+    void update(City city);
+
+    void deleteById(int id);
 }
--- src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/implementation/CityServiceImpl.java
+++ src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/implementation/CityServiceImpl.java
@@ -1,4 +1,41 @@
 package com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.implementation;
 
-public class CityServiceImpl {
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.model.City;
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.repository.CityRepository;
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.abstraction.CityService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CityServiceImpl implements CityService {
+    private final CityRepository cityRepository;
+
+    public CityServiceImpl(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
+    @Override
+    public List<City> getAll() {
+        return cityRepository.findAll();
+    }
+
+    @Override
+    public Optional<City> getById(int id) {
+        return cityRepository.findById(id);
+    }
+
+    @Override
+    public void add(City city) {
+        cityRepository.save(city);
+    }
+
+    @Override
+    public void update(City city) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+        cityRepository.deleteById(id);
+    }
 }
