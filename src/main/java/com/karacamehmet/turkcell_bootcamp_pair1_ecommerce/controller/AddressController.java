4dceb08b3950bab9711fa0e36839b9a472919597
karaca <mekaraca18@gmail.com>
2024-03-07 21:57:28 +0300

    ...

---
+++ src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/controller/AddressController.java
@@ -0,0 +1,24 @@
+package com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.controller;
+
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.model.Address;
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.abstraction.AddressService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/addresses")
+public class AddressController {
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @GetMapping()
+    List<Address> getAll() {
+        return addressService.getAll();
+    }
+}
