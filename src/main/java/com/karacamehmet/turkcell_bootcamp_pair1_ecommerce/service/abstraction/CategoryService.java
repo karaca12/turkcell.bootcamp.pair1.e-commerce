8da7f96c9efc5bf6995421fcf12d53fd83fb546c
karaca <mekaraca18@gmail.com>
2024-03-07 21:55:43 +0300

    ...

--- src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/abstraction/CategoryService.java
+++ src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/abstraction/CategoryService.java
@@ -1,4 +1,18 @@
 package com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.abstraction;
 
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.model.Category;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryService {
+    List<Category> getAll();
+
+    Optional<Category> getById(int id);
+
+    void add(Category category);
+
+    void update(Category category);
+
+    void deleteById(int id);
 }
--- src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/implementation/CategoryServiceImpl.java
+++ src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/service/implementation/CategoryServiceImpl.java
@@ -1,4 +1,42 @@
 package com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.implementation;
 
-public class CategoryServiceImpl {
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.model.Category;
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.repository.CategoryRepository;
+import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.abstraction.CategoryService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Category> getById(int id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public void add(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void update(Category category) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+        categoryRepository.deleteById(id);
+    }
 }
