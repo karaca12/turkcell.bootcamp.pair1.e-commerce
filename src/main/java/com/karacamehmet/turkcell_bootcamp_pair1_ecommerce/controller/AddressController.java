017b29f84539d179d61b065ff962f7775ae2fb3a
karaca <mekaraca18@gmail.com>
2024-03-07 22:24:57 +0300

    AddressController

--- src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/controller/AddressController.java
+++ src/main/java/com/karacamehmet/turkcell_bootcamp_pair1_ecommerce/controller/AddressController.java
@@ -2,11 +2,10 @@ package com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.controller;
 
 import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.model.Address;
 import com.karacamehmet.turkcell_bootcamp_pair1_ecommerce.service.abstraction.AddressService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/addresses")
@@ -20,5 +19,20 @@ public class AddressController {
     @GetMapping()
     List<Address> getAll() {
         return addressService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Address> getById(@PathVariable int id) {
+        return addressService.getById(id);
+    }
+
+    @PostMapping
+    void add(@RequestBody Address address) {
+        addressService.add(address);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteById(@PathVariable int id) {
+        addressService.deleteById(id);
     }
 }
